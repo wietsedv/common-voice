0af0144bba6e189177bec31dca992fda3df06200
Wietse de Vries <wietsedv@proton.me>
2025-07-07 10:06:34 +0200

    callback url workaround

--- server/src/infrastructure/authentication/authentication.ts
+++ server/src/infrastructure/authentication/authentication.ts
@@ -6,7 +6,8 @@ export const COMMON_VOICE_DOMAIN_MAP: Readonly<Record<Environment, string>> = {
   prod: 'https://commonvoice.mozilla.org',
   stage: 'https://commonvoice.allizom.org',
   sandbox: 'https://sandbox.commonvoice.allizom.org',
-  local: 'http://localhost:9000', // TODO: use SERVER_PORT
+  // local: 'http://localhost:9000', // TODO: use SERVER_PORT
+  local: 'https://praoten.nl'
 }
 
 export const callbackURL = (env: Environment) =>
