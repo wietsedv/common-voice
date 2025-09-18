6eaf2ef87c88206d9d643ad4b256369306307666
Wietse de Vries <wietsedv@proton.me>
2025-09-18 11:00:50 +0200

    tekst homepage

--- web/src/components/pages/home/hero-section/index.tsx
+++ web/src/components/pages/home/hero-section/index.tsx
@@ -1,51 +1,53 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
-import React from 'react'
-import { Localized } from '@fluent/react'
-import classNames from 'classnames'
 
-import { LinkButton } from '../../../ui/ui'
-import { ArrowRight, MicIcon, Play } from '../../../ui/icons'
+import { Localized } from "@fluent/react";
+import classNames from "classnames";
+import type React from "react";
+import { usePreloadImage } from "../../../../hooks/use-preload-image";
+import URLS from "../../../../urls";
+import { LocaleLink } from "../../../locale-helpers";
+import { Map } from "../../../map";
+import { ArrowRight, MicIcon, Play } from "../../../ui/icons";
+import { LinkButton } from "../../../ui/ui";
 
-import {Map} from "../../../map"
-
-import { LocaleLink } from '../../../locale-helpers'
-import URLS from '../../../../urls'
-
-import { usePreloadImage } from '../../../../hooks/use-preload-image'
-
-import './hero-section.css'
+import "./hero-section.css";
 
 type Props = {
-  isLocaleContributable: boolean
-}
+	isLocaleContributable: boolean;
+};
 
 export const HeroSection: React.FC<Props> = ({ isLocaleContributable }) => {
-  // const imageUrl = require('./assets/hero-hd.webp')
-  // const isLoaded = usePreloadImage(imageUrl)
+	// const imageUrl = require('./assets/hero-hd.webp')
+	// const isLoaded = usePreloadImage(imageUrl)
 
-  const linkDisabled = !isLocaleContributable
+	const linkDisabled = !isLocaleContributable;
 
-  return (
-    <section className="hero-section">
-      <div className="hero-container">
-        <div className="hero-text-wrapper">
-          <div className="hero-text-container">
-            <h1>Technologie die Nedersaksisch spreekt</h1>
-            <Localized id="hero-subtitle-1" elems={{ bold: <b /> }}>
-              <p />
-            </Localized>
-            <Localized id="hero-subtitle-2">
-              <p />
-            </Localized>
-            {/* <Localized id="get-started" elems={{ icon: <ArrowRight /> }}>
+	return (
+		<section className="hero-section">
+			<div className="hero-container">
+				<div className="hero-text-wrapper">
+					<div className="hero-text-container">
+						<h1>Technologie die Nedersaksisch spreekt</h1>
+						<Localized id="hero-subtitle-1" elems={{ bold: <b /> }}>
+							<p />
+						</Localized>
+						<Localized id="hero-subtitle-2">
+							<p />
+						</Localized>
+						<p>
+							<br/>Klik hiernaast/hieronder op de naam van het gebied waarvan je de
+							lokale variant spreekt, om jouw stem op te nemen en jouw taal te
+							helpen!
+						</p>
+						{/* <Localized id="get-started" elems={{ icon: <ArrowRight /> }}>
               <LinkButton rounded to={`${URLS.ROOT}?page=home#action-items`} />
             </Localized> */}
-          </div>
-        </div>
-        <div className="hero-image-container">
-          <Map />
-        </div>
-      </div>
-    </section>
-  )
-}
+					</div>
+				</div>
+				<div className="hero-image-container">
+					<Map />
+				</div>
+			</div>
+		</section>
+	);
+};
